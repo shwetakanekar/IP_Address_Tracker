82945698f55c5f06761c2d11623886f4cc1e58ec
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-02-09 19:44:01 +0530

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  return <div>IP Address Tracker</div>;
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
