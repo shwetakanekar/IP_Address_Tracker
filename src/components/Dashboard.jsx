46dc5c14ab8da963b5ed4d7ecb31076d8171bdc1
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-02-10 11:51:53 +0530

    Created search for IP address

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,17 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <h1 className="header">IP Address Tracker</h1>
+      <form>
+        <input
+          type="text"
+          name="ipAddress"
+          id="ipAddress"
+          placeholder="Search for any IP address or domain"
+        />
+        <button type="submit">
+          <img src="/public/icon-arrow.svg" alt="Arrow icon" />
+        </button>
+      </form>
     </div>
   );
 };
